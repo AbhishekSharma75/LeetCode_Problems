5f07af82338807a26c6449621bd12d4ff2671014
Abhishek Sharma <85792918+abhishek751982@users.noreply.github.com>
2023-06-05 15:26:05 +0530

    Time: 26 ms (56.72%), Space: 19.2 MB (78.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        vector<int> temp;
+        // Optima solution
+        int j = -1;
         for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] == 0) {
+                j = i;
+                break;
+            }   
+        }
+        if(j == -1) return;
+        for(int i = j+1; i < arr.size(); i++) {
             if(arr[i] != 0) {
-                temp.push_back(arr[i]);
+                swap(arr[i], arr[j]);
+                j++;
             }
-        }
-        int nz = temp.size();
-        for(int i = 0; i < temp.size(); i++) {
-            arr[i] = temp[i];
-        }
-        for(int i = nz; i < arr.size(); i++){
-            arr[i] = 0;
         }
     }
 };
