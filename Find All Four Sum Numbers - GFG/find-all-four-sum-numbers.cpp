bb8e09d79d4c45ec6a92ccede1edb656d604ac32
Abhishek Sharma <85792918+abhishek751982@users.noreply.github.com>
2023-06-19 00:28:56 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -8,29 +8,24 @@ class Solution{
     public:
     vector<vector<int> > fourSum(vector<int> &arr, int target) {
         int n = arr.size();
-        vector<vector<int>> ans;
+        set<vector<int>> st;
         sort(arr.begin(), arr.end());
         
         for(int i = 0; i < n; i++) {
-            if(i > 0 && arr[i] == arr[i-1]) continue;
             for(int j = i+1; j < n; j++) {
-                if(j > i+1 && arr[j] == arr[j-1]) continue;
-                
+                // use two pointers
                 int k = j+1;
                 int l = n-1;
-                
                 while(k < l) {
-                    int sum = arr[i];
+                    long long sum = arr[i];
                     sum += arr[j];
                     sum += arr[k];
                     sum += arr[l];
                     if(sum == target) {
                         vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
-                        ans.push_back(temp);
+                        st.insert(temp);
                         k++;
                         l--;
-                        while(k < l && arr[k] == arr[k-1]) k++;
-                        // while(k < l && arr[l] == arr[l-1]) l--;
                     }
                     else if(sum < target) {
                         k++;
@@ -39,6 +34,7 @@ class Solution{
                 }
             }
         }
+        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
