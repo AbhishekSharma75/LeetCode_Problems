aa82d43b93dbfb52e2ba45c9c15b540ae688a27a
Abhishek Sharma <85792918+abhishek751982@users.noreply.github.com>
2023-06-22 11:11:46 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -5,38 +5,22 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    private:
-    void swapIfGreater(long long arr1[], long long arr2[], int ind1, int ind2) {
-        if(arr1[ind1] > arr2[ind2]) {
-            swap(arr1[ind1], arr2[ind2]);
-        }
-    }
+    
     public:
         void merge(long long arr1[], long long arr2[], int n, int m) 
-        { 
-            int len = (n + m);
-            int gap = (len / 2) + (len % 2);
-            while(gap > 0) {
-                int left = 0;
-                int right = left + gap;
-                while(right < len) {
-                    // left in arr1[] and right in aar2[]
-                    if(left < n && right >= n) {
-                        swapIfGreater(arr1, arr2, left, right - n);
-                    }
-                    // both pointers in arr2[]
-                    else if(left >= n) {
-                        swapIfGreater(arr2, arr2, left - n, right - n);
-                    }
-                    // both pointers in arr1[]
-                    else {
-                        swapIfGreater(arr1, arr1, left, right);
-                    }
-                    left++, right++;
+        {
+            int i = n-1, j = 0;
+            while (i>=0 and j<m)
+            {
+                if (arr1[i] > arr2[j])
+                {
+                    swap(arr1[i], arr2[j]);
+                    i--, j++;
                 }
-                if(gap == 1) break;
-                gap = (gap / 2) + (gap % 2);
+                else
+                    break;
             }
+            sort(arr1, arr1+n); sort(arr2, arr2+m);
         } 
 };
 
