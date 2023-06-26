669abfabebdc1a95cf80d68630fdfd640c3dc8ad
Abhishek Sharma <85792918+abhishek751982@users.noreply.github.com>
2023-06-26 22:15:27 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -8,17 +8,18 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int bs(int arr[], int low, int high, int k) {
-        if(low > high) return -1;
-        int mid = (low + high) / 2;
-        if(arr[mid] == k) return mid;
-        else if(k > arr[mid])
-            return bs(arr, mid+1, high, k);
-        else
-            return bs(arr, low, mid-1, k);
-    }
     int binarysearch(int arr[], int n, int k) {
-        return bs(arr, 0, n-1, k);
+        int low = 0, high = n-1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] == k) 
+                return mid;
+            else if(k > arr[mid]) 
+                low = mid + 1;
+            else 
+                high = mid - 1;
+        }
+        return -1;
     }
 };
 
